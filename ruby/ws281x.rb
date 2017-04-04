3ea712ff91390aa3436e4bba7495debdd7817be2
Mate Farkas <mate.farkas@sch.hu>
2017-04-04 21:02:15 +0000

    WS281X has been refectored.

--- ruby/ws281x.rb
+++ ruby/ws281x.rb
@@ -1,10 +1,19 @@
 require 'ffi'
 
-module WS281X
+class WS281X
   extend FFI::Library
 
+  FREQUENCY = 800000
+  GPIO = 18
+  DMA = 5
+  WIDTH = 8
+  HEIGHT = 8
+  BRIGHTNESS = 255
   RPI_PWM_CHANNELS = 2
-  
+  WS2811_STRIP_RGB = 0x00100800
+
+  @config = nil
+
   class Ws2811_led_t < FFI::Struct
     layout \
       :blue,  :uint8,
@@ -61,93 +70,79 @@ module WS281X
   attach_function :ws2811_render, [ Ws2811_t ], :int
   attach_function :ws2811_get_return_t_str, [ :ws2811_return_t ], :string
 
-  def self.dump(config)
-    print "DEVICE:  #{config[:device]}\n"
-    print "RPI_HW:  #{config[:rpi_hw]}\n"
-    print "FREQ:    #{config[:freq]}\n"
-    print "DMANUM:  #{config[:dmanum]}\n"
-    print "CHANNEL: #{config[:channel]}\n"
-    config[:channel].each_with_index { |channel, index|
-      print "  [#{index}] GPIONUM:    #{channel[:gpionum]}\n"
-      print "  [#{index}] INVERT:     #{channel[:invert]}\n"
-      print "  [#{index}] COUNT:      #{channel[:count]}\n"
-      print "  [#{index}] STRIP_TYPE: #{channel[:strip_type]}\n"
-      print "  [#{index}] BRIGHTNESS: #{channel[:brightness]}\n"
-      print "  [#{index}] WSHIFT:     #{channel[:wshift]}\n"
-      print "  [#{index}] RSHIFT:     #{channel[:rshift]}\n"
-      print "  [#{index}] GSHIFT:     #{channel[:gshift]}\n"
-      print "  [#{index}] BSHIFT:     #{channel[:bshift]}\n"
-      print "  [#{index}] LEDS:       #{channel[:leds]}\n"
-      channel[:count].times { |i|
-        led = Ws2811_led_t.new channel[:leds][i]
-        print "    [#{i}] : #{led[:red]},#{led[:green]},#{led[:blue]}\n"
-      }
-    }
-  end
-
-  def self.test
+  def initialize
     channel0_pointer = WS281X.malloc Ws2811_channel_t.size
-    channel0 = Ws2811_channel_t.new channel0_pointer
-    channel0[:gpionum] = 18
-    channel0[:count] = 64
-    channel0[:invert] = 0
-    channel0[:brightness] = 255
-    channel0[:strip_type] = 0x00100800
-    
     channel1_pointer = WS281X.malloc Ws2811_channel_t.size
+    config_pointer = WS281X.malloc Ws2811_t.size
+
+    ObjectSpace.define_finalizer(self, proc {
+      if not @config.nil?
+        WS281X.ws2811_fini @config
+        @config = nil
+      end
+
+      WS281X.free channel0_pointer
+      WS281X.free channel1_pointer
+      WS281X.free config_pointer
+    })
+
+    channel0 = Ws2811_channel_t.new channel0_pointer
+    channel0[:gpionum] = GPIO
+    channel0[:count] = WIDTH * HEIGHT
+    channel0[:invert] = 0
+    channel0[:brightness] = BRIGHTNESS
+    channel0[:strip_type] = WS2811_STRIP_RGB
+    
     channel1 = Ws2811_channel_t.new channel1_pointer
     channel1[:gpionum] = 0
     channel1[:count] = 0
     channel1[:invert] = 0
     channel1[:brightness] = 0
     
-    config_pointer = WS281X.malloc Ws2811_t.size
-    config = Ws2811_t.new config_pointer
-    config[:freq] = 800000
-    config[:dmanum] = 5
-    config[:channel][0] = channel0
-    config[:channel][1] = channel1
+    @config = Ws2811_t.new config_pointer
+    @config[:freq] = FREQUENCY
+    @config[:dmanum] = DMA
+    @config[:channel][0] = channel0
+    @config[:channel][1] = channel1
     
-    init_code = WS281X.ws2811_init config
-    print "#{WS281X.ws2811_get_return_t_str init_code}\n"
+    init_code = WS281X.ws2811_init @config
     
-    dump config
+    if init_code != :WS2811_SUCCESS
+      raise "#{WS281X.ws2811_get_return_t_str init_code}"
+    end
+  end
+
+  def set_pixel_color(row, col, red, green, blue)
+    led = Ws2811_led_t.new @config[:channel][0][:leds][(row * 8 + col) * Ws2811_led_t.size]
+    led[:red]   = red
+    led[:green] = green
+    led[:blue]  = blue
+  end
+
+  def clear
+    HEIGHT.times { |row|
+      WIDTH.times { |col|
+        set_pixel_color(row, col, 0,0,0)
+      }
+    }
+  end
+
+  def render
+    WS281X.ws2811_render @config
+  end
+
+  def self.test
+    ws281x = WS281X.new
     
-    led0 = Ws2811_led_t.new config[:channel][0][:leds][0 * Ws2811_led_t.size]
-    led0[:red]   = 255
-    led0[:green] = 0
-    led0[:blue]  = 0
-    
-    led1 = Ws2811_led_t.new config[:channel][0][:leds][1 * Ws2811_led_t.size]
-    led1[:red]   = 0
-    led1[:green] = 255
-    led1[:blue]  = 0
-    
-    led2 = Ws2811_led_t.new config[:channel][0][:leds][2 * Ws2811_led_t.size]
-    led2[:red]   = 0
-    led2[:green] = 0
-    led2[:blue]  = 255
-    
-    WS281X.ws2811_render config
+    ws281x.set_pixel_color(0,0, 255,0,0)
+    ws281x.set_pixel_color(0,1, 0,255,0)
+    ws281x.set_pixel_color(0,2, 0,0,255)
+
+    ws281x.render
     
     sleep 1
-
-    led0[:red]   = 0
-    led0[:green] = 0
-    led0[:blue]  = 0
-    led1[:red]   = 0
-    led1[:green] = 0
-    led1[:blue]  = 0
-    led2[:red]   = 0
-    led2[:green] = 0
-    led2[:blue]  = 0
-    WS281X.ws2811_render config
-
-    WS281X.ws2811_fini config
-
-    WS281X.free channel0_pointer
-    WS281X.free channel1_pointer
-    WS281X.free config_pointer
+    ws281x.clear
+    ws281x.render
   end
 end
 
